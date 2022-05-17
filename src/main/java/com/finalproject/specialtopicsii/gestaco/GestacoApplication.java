d177439c6c1ccb6afe39997f56c3904605e3cab4
Jonatan Gabriel Bortolon <jonatanbortolon@MacBook-Pro-2.local>
2022-05-17 12:20:08 -0300

    Spring Boot added 🍃🍃

---
+++ src/main/java/com/finalproject/specialtopicsii/gestaco/GestacoApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.specialtopicsii.gestaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/specialtopicsii/gestaco/GestacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.specialtopicsii.gestaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
