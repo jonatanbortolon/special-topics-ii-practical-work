4678e4677a5e600ff502b27fcc6020b6aa2ff49d
Lucas Oliveira Damaceno <lucasod123@gmail.com>
2022-05-23 11:21:13 -0300

    Account class added

--- src/main/java/com/finalproject/specialtopicsii/gestaco/GestacoApplication.java
+++ src/main/java/com/finalproject/specialtopicsii/gestaco/GestacoApplication.java
@@ -2,6 +2,7 @@ package com.finalproject.specialtopicsii.gestaco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Set;
 
 @SpringBootApplication
 public class GestacoApplication {
