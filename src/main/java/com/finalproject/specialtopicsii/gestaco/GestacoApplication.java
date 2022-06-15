7bed5f07c7ae7a1220da9ecc6230a60f219d9230
Jonatan Gabriel Bortolon <jonatangabrielbortolon@outlook.com>
2022-06-15 12:17:14 -0300

    ❗GABRIEL AJUDOU A FAZER A ETAPA 3 TAMBÉM❗

--- src/main/java/com/finalproject/specialtopicsii/gestaco/GestacoApplication.java
+++ src/main/java/com/finalproject/specialtopicsii/gestaco/GestacoApplication.java
@@ -2,7 +2,6 @@ package com.finalproject.specialtopicsii.gestaco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Set;
 
 @SpringBootApplication
 public class GestacoApplication {
